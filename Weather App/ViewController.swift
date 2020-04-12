027743f4dba67787eb9379f37aa0292ea7cbef53
nomansyed19 <56279785+nomansyed19@users.noreply.github.com>
2020-04-11 19:22:38 -0500

    Created UI, created IB actions for search button, created IB outlets for search button, temperature label, search bar.

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    // This method runs right when the app is loaded
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
 
-
+    
+    // run this method after use pressed search button
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)    // close the keyBoard
+        
+    }
+    
+    // run this method after user presses search button on they keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if(searchTextField.text != nil){    // if textfield is empty then print and close the keyboard
+            print(searchTextField.text!)
+            searchTextField.endEditing(true)    // close the keyBoard
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    // run this fuction after user is done editing text field
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = "";      // clears the textbox after finished editing
+    }
+    
 }
-
